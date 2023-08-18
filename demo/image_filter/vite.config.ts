9fbc959877e3916c18546becf145cbedcc5f8245
zyc <239559238@qq.com>
2023-08-18 09:20:39 +0800

    整合demo

--- demo/image_filter/vite.config.ts
+++ demo/image_filter/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { URL, fileURLToPath } from 'url'
 import vue from '@vitejs/plugin-vue'
-import manifest from './manifest.json'
+import manifest from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
     treeShaking: true
   },
   server: {
-    port: manifest.port
+    port: manifest.devProp
   },
   build: {
     chunkSizeWarningLimit: 2048,
--- packages/main/src/pages/dashboard/index.vue
+++ packages/main/src/pages/dashboard/index.vue
@@ -3,8 +3,17 @@
     <Sider />
     <Container>
       <div class="grid gap-6 grid-cols-auto-fill-300">
-        <div class="p-4 bg-white rounded-md shadow-md flex h-44">123</div>
-        <div class="p-4 bg-white rounded-md shadow-md flex h-44">123</div>
+        <a
+          v-for="demo in demoList"
+          target="_blank"
+          :href="demo.uri"
+          :key="demo.name"
+          class="p-4 rounded-md shadow-md flex items-center justify-center text-xl cursor-pointer h-44 hover:shadow-lg"
+          :style="{ backgroundColor: hex }"
+          v-memo="[]"
+        >
+          {{ demo.name }}
+        </a>
       </div>
       <template #footer>
         <Footer />
@@ -14,7 +23,30 @@
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
+import { onBeforeMount } from 'vue'
 import Sider from './components/Sider.vue'
 import Footer from './components/Footer.vue'
 import Container from './components/Container.vue'
+
+const demoList = ref<{ name: string; uri: string }[]>([])
+const packages = import.meta.glob('../../../../../demo/**/package.json', { eager: true })
+const isDev = import.meta.env.DEV
+const hex = computed(() => {
+  return `#${Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padEnd(6, '0')}`
+})
+onBeforeMount(() => {
+  const keys = Object.keys(packages)
+  for (const key of keys) {
+    const demo = packages[key] as { description: string; name: string; devProp: number }
+    demoList.value.push({
+      name: demo.description,
+      uri: isDev
+        ? `http://localhost:${demo.devProp}`
+        : `./demo/${demo.name?.replace('@tetap-demo/', '')}`
+    })
+  }
+})
 </script>
